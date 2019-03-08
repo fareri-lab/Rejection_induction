9ff6122f3fa2fad1c6d0e9e8200f7ba19c681a11
dfareri <dfareri@users.noreply.github.com>
2019-03-08 09:46:23 -0500

    Rej_01

--- practice/SocaMRI_lastrun.py
+++ practice/SocaMRI_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy2 Experiment Builder (v1.85.2),
-    on March 04, 2019, at 15:19
+    on March 08, 2019, at 09:09
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
